5e379a5abd5256c4c3957787c5c26ebe7b1b8deb
Deepak Prabhakara <deepak.prabhakara@gmail.com>
2016-05-23 16:12:37 +0100

    run tests as user 7438:7438

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ CONTAINER_NAME="pysbx"
 
 
 docker rm -f $CONTAINER_NAME || true
-docker run -u 7438 -d \
+docker run -u 7438:7438 -d \
 	-v /tmp:/run/sandbox/ipc \
 	--name $CONTAINER_NAME \
 	$IMAGE run.py 0 1
@@ -15,4 +15,4 @@ docker run -u 7438 -d \
 echo 'sandbox is running'
 
 echo 'starting test driver'
-docker run -u 7438 -it --rm -v /tmp:/run/sandbox/ipc --rm thschroeter/sandbox-python test_server.py 0 1
+docker run -u 7438:7438 -it --rm -v /tmp:/run/sandbox/ipc --rm thschroeter/sandbox-python test_server.py 0 1

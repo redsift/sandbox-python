5ca6344f30a6e9d3afc8b79f8d56e8890bb7f0ec
Deepak Prabhakara <deepak@redsift.io>
2018-08-09 14:03:36 +0100

    perform pip install only once for each unique path

--- root/usr/bin/redsift/install.py
+++ root/usr/bin/redsift/install.py
@@ -26,14 +26,17 @@ sr = init.env_var_or_exit('SIFT_ROOT')
 sj = init.env_var_or_exit('SIFT_JSON')
 ir = init.env_var_or_exit('IPC_ROOT')
 
+cache = {}
+
 sift = json.load(open(os.path.join(sr, sj)))
 for n in sift['dag']['nodes']:
     if 'implementation' in n and 'python' in n['implementation']:
         d = os.path.dirname(n['implementation']['python'])
         requirements_file = os.path.join(sr, d, 'requirements.txt')
-        if os.path.exists(requirements_file):
+        if os.path.exists(requirements_file) and requirements_file not in cache:
             td = os.path.join(sr, d, 'site-packages')
             ret = pipmain(['install', '--target='+td, '-r', requirements_file])
+            cache[requirements_file]=1
             if ret != 0:
                 print('pip install returned code: %s' % ret)
                 sys.exit(ret)
